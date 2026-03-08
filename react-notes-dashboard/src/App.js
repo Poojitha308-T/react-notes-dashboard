4dad6c9598bcc9190fa3070fc761fbfa8a346958
Poojitha308-T <poojithathadiboyina@gmail.com>
2026-03-08 16:44:33 +0530

    first commit

--- react-notes-dashboard/src/App.js
+++ react-notes-dashboard/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { NotesProvider } from "./context/NotesContext";
+import NotesCount from './components/NotesCount';
+import AddNote from "./components/AddNote";
+import NotesList from './components/NotesList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function App(){
+  return(
+    <NotesProvider>
+      <div className="App">
+        <h1>Notes Dashboard</h1>
+
+        <AddNote/>
+
+        <NotesCount/>
+
+        <NotesList/>
+
+      </div>
+    </NotesProvider>
   );
 }
 
