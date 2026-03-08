f0ee2c3f82440a39545d7e9147c74a49180c669f
Poojitha308-T <poojithathadiboyina@gmail.com>
2026-03-08 16:40:01 +0530

    first commit

---
+++ react-notes-dashboard/src/components/NotesList.js
@@ -0,0 +1,23 @@
+import React, { useContext } from "react";
+import { NotesContext } from './../context/NotesContext';
+
+function NotesList() {
+    const { notes, selectNote, setSelectNote } = useContext(NotesContext);
+
+    return(
+        <div>
+            <h3>Notes</h3>
+
+            <ul>
+                {notes.map((note) => (
+                    <li key={note.id} onClick={() => setSelectNote(note.id)}
+                    style={{cursor: "pointer", padding: "5px"}}>
+                        {note.text};
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default NotesList;
